567e20d5d065cb2a01764d2b54b8a45bdc3eb6cd
SaranyaSusi <114824657+SaranyaSusi@users.noreply.github.com>
2022-10-01 22:21:23 +0530

    Add files via upload
    day1

---
+++ Car.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Car {
+	int car = 5;
+	byte carNumber = 125 ;
+	short carPrice = 32757;
+	int carDoor = 01234567;
+	long carEngineNumber = 8596458788L ;
+	float carDoors = 7865.9245F ;
+	double carSeat = 524685452.8784512 ;
+	char carName = 'S';
+	boolean carModel = false;
+
+}
---
+++ FactorialNumber.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class FactorialNumber
+{
+	 public static void main(String[] args) 
+	 {
+		 int a = 5;
+		 int fact = 1; 
+		 for (int i=1; i<=a; i++) 
+		 {
+			 fact=fact*i;
+		 }
+		 System.out.println("The factorial number of 5 is : "+fact );
+	}
+}
+
+
+// What are my learnings from this code?
+//  1. Writing a code on my own
+//  2. Understood the syntax
+//  3. Understood the loop concept clearly
